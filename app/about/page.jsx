948fde8b400fabc404a58607a62bf1133904d24a
dig33 <119484187+LKerin822@users.noreply.github.com>
2023-05-17 19:59:19 +1000

    wed night

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -18,9 +18,7 @@ const AboutPage = () => {
         <br></br>
         <p>Hi, I'm Louise Kerin. I am an artist and designer based in Langwarrin, on the Mornington Peninsula, in Victoria.  
           <br></br>
-          <br></br>My area of interest in art is around portraiture.  My portraiture journey began in graphite pencils which is a medium I still love to return to.  From there I moved into soft pastel and then acrylics.  The past few years I have mostly focused on creating digital art.  The main appeal with digital art is the portablily of materials.  I can (and do!) take my iPad and pencil with me everywhere.  
-          <br></br>
-          <br></br>My design journey has taken me to studying for a Bachelor degree online with Curtin University. Should everything run smoothly, I should graduate later this year with a Bachelor of Arts: (Digital Experience and Interaction Design) and (Professional Writing and Publishing).</p>
+          <br></br>My area of interest in art is around portraiture.  My portraiture journey began in graphite pencils which is a medium I still love to return to.  From there I moved into soft pastel and then acrylics.  The past few years I have mostly focused on creating digital art.  The main appeal with digital art is the portablily of materials.  I can (and do!) take my iPad and pencil with me everywhere.  My design journey has taken me to studying for a Bachelor degree online with Curtin University. Should everything run smoothly, I should graduate later this year with a Bachelor of Arts: (Digital Experience and Interaction Design) and (Professional Writing and Publishing).</p>
         </Col>
 
       <Col md={5}>
