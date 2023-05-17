121122219cb4fc791a47635a6fd4fbcff944e796
dig33 <119484187+LKerin822@users.noreply.github.com>
2023-05-17 16:05:15 +1000

    update

--- components/DesignCaro.jsx
+++ components/DesignCaro.jsx
@@ -49,7 +49,7 @@ export default function DesignCaro({ children }) {
                 </Col>
                 <Col>
                 <br></br>
-                    <h3 class="caro-title">The Platypus Project <br></br>
+                    <h3 className="caro-title">The Platypus Project <br></br>
                     <p><i>(Digital Illustration)</i></p></h3>
                     <p class="caro-text">
                     This logo was recently commissioned to accompany the book release of 'The Platypus Principle.''
@@ -64,74 +64,10 @@ export default function DesignCaro({ children }) {
                 
             </Carousel.Item>
 
-            <Carousel.Item  interval={4000}>
-            <br></br>
-            <Row sm={1}> <h2 class="text-center">Design Gallery</h2></Row>
-            <Row><br></br></Row>
-            <Row>
-              <Col sm={1}></Col>
-              <Col>
-                <img src="/Leslie.png"  alt="slides" />
-                </Col>
-                <Col>
-                <br></br>
-                    <h3 class="caro-title">Leslie Knope <br></br>
-                    <p><i>(Digital Illustration)</i></p></h3>
-                    <p class="caro-text">
-                    This portait of Leslie Knope was pet project created in 2019.  I have been a fan of Parks and Recreation for many years and had always wanted to draw her. This piece is the result of many hours work flying home from New York.  <br></br>  
-                       (2019)</p>
-                       <Link className='btn btn-primary btn-sml my-3' href="/gallery">Back to all Galleries</Link>
-                  </Col>
-                  <Col sm={1}></Col>
-                    </Row>   
+            
                 
-            </Carousel.Item>
-            <Carousel.Item  interval={4000}>
-            <br></br>
-            <Row sm={1}> <h2 class="text-center">Design Gallery</h2></Row>
-            <Row><br></br></Row>
-            <Row>
-              <Col sm={1}></Col>
-              <Col>
-                <img src="/royal-wedding.png"  alt="slides" />
-                </Col>
-                <Col>
-                <br></br>
-                    <h3 class="caro-title">Royal Wedding of William and Kate <br></br>
-                    <p><i>(Digital Illustration)</i></p></h3>
-                    <p class="caro-text">
-                    This digital illustration was created in 2021 to mark the tenth anniversary of the royal wedding between Prince William and Kate Middleton.  Back in 2011, I created this same image in soft pastel for the art shop I was working for at the time. I often like to recreate earlier pieces to see how much I have learned over the years.   <br></br>  
-                       (2021)</p>
-                       <Link className='btn btn-primary btn-sml my-3' href="/gallery">Back to all Galleries</Link>
-                  </Col>
-                  <Col sm={1}></Col>
-                    </Row>   
-                
-            </Carousel.Item>
-
-            <Carousel.Item  interval={4000}>
-            <br></br>
-            <Row sm={1}> <h2 class="text-center">Design Gallery</h2></Row>
-            <Row><br></br></Row>
-            <Row>
-              <Col sm={1}></Col>
-              <Col>
-                <img src="/jill-biden.png"  alt="slides" />
-                </Col>
-                <Col>
-                <br></br>
-                    <h3 class="caro-title">Jill Biden<br></br>
-                    <p><i>(Digital Illustration)</i></p></h3>
-                    <p class="caro-text">
-                    This portrait of Jill Biden, the First Lady of the USA, was a covid lockdown project of 2021.  Living in Melbourne, we experienced many lockdowns which were the perfect excuse to pick up my pen and draw!  <br></br>  
-                       (2021)</p>
-                       <Link className='btn btn-primary btn-sml my-3' href="/gallery">Back to all Galleries</Link>
-                  </Col>
-                  <Col sm={1}></Col>
-                    </Row>   
-                
-            </Carousel.Item>
-
+            
+               
 
 
     </Carousel>
