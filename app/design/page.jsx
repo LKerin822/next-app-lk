85b894ba34d0bbefc2dd22e49dcdddd10e8ec6bd
dig33 <119484187+LKerin822@users.noreply.github.com>
2023-05-17 16:10:13 +1000

    test

--- app/design/page.jsx
+++ app/design/page.jsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from 'react';
 import {Card, Row, Col, Carousel} from 'react-bootstrap';
 import Link from "next/link";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Image from "next/image";
-import DesignCaro from '@/components/DesignCaro';
+
+
 import React from 'react'
 
 const designPage = () => {
   return (
-    <DesignCaro />
+    <h1>Test</h1>
   )
-}
+};
 
-export default designPage
+export default designPage;
