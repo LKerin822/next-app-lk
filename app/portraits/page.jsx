bd4530b636ff547f7bce6fcd144b3507211a3f17
dig33 <119484187+LKerin822@users.noreply.github.com>
2023-05-17 15:32:14 +1000

    portrait commit

--- app/portraits/page.jsx
+++ app/portraits/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+
+'use client'; 
+import { useEffect, useState } from 'react';
+import {Card, Row, Col, Carousel} from 'react-bootstrap';
+import Link from "next/link";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from "next/image";
+import PortraitCaro from '@/components/PortraitCaro';
 
 const portraitPage = () => {
   return (
-    <div>portraitPage</div>
-  )
+    <PortraitCaro/>
+    )
 }
 
 export default portraitPage
